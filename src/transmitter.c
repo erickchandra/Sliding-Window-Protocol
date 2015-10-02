a0ed882a34923a6e2909d9d801a71f15d984c52d
Erick Chandra <erickchandra.1@gmail.com>
2015-10-02 22:25:01 +0700

    第一次提交，创新发射器与接收器。First commit, created transmitter and receiver.

---
+++ src/transmitter.c
@@ -0,0 +1,31 @@
+/* File name			: transmitter.c
+ * Author				: Erick Chandra
+ * Author email			: erickchandra.1@gmail.com
+ * Colleague 1			: Candy Olivia Mawalim
+ * Colleague 1 email	: 
+ * Colleague 2			: Ignatius Alriana Moel
+ * Colleague 3 email	: 
+ * Description			: This program is created to complete Computer Networks course assignment.
+ * Topic				: Sliding Window Protocol + flow control and error control
+ * References			: Found in the document
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+// Global dictionaries
+
+
+// Function Declaration
+
+
+// Main program
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
+
+// Function Definition Realisation
